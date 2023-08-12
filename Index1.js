881b4a54faa986ae4ae5c78000088c86c09325f6
riyasaxena09 <135497287+riyasaxena09@users.noreply.github.com>
2023-08-12 22:40:09 +0530

    add

---
+++ Index1.js
@@ -0,0 +1,37 @@
+function Index1(){
+    return(
+<>
+<div>
+The Generics
+<button>Get out Latest Album</button>
+
+</div>
+<div>Tours</div>
+<div>
+    <span>July 16</span>
+    <span>BUDWEISER STAGE</span>
+    <button>Buy Ticket</button>
+</div>
+<div>
+    <span>July 16</span>
+    <span>BUDWEISER STAGE</span>
+    <button>Buy Ticket</button>
+   
+</div>
+<div>
+    <span>July 16</span>
+    <span>BUDWEISER STAGE</span>
+    <button>Buy Ticket</button>
+</div>
+<div>
+    <span>July 16</span>
+    <span>BUDWEISER STAGE</span>
+    <button>Buy Ticket</button>
+</div>
+<footer>
+    The generics
+</footer>
+</>
+    )
+}
+export default Index1;
