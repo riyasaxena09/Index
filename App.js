cc7d1a3bbc78b1c8166c9c5d1f98ecd5de44fbe1
riyasaxena09 <135497287+riyasaxena09@users.noreply.github.com>
2023-08-13 15:41:39 +0530

    add app

--- App.js
+++ App.js
@@ -1,13 +1,82 @@
+import React, { useState,useEffect,useCallback } from 'react';
 
-import React, { useState } from "react";
-import List from "./List";
-function App(){
+import MoviesList from './components/MoviesList';
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  const [movie,setm]=useState([]);
+    const [load,setload]=useState(false);
+    const [error,setE]=useState(null);
+const pro=useCallback(async function(){
+  setload(true);
+
+  setE(null);
+  try{
+     const a=await fetch('https://movie-aab57-default-rtdb.firebaseio.com/movies.json');
+     if(!a.ok){
+      throw new Error('somehtinng went wrong')
+     }
+             const b=await a.json();
+             const loaded=[];
+             for(let key in b){
+              loaded.push({
+                id:key,
+title:b[key].Title,
+openingText:b[key].Text,
+releasingDate:b[key].Date,
+              })
+             }
+        console.log(b)
+          
+              setm(loaded);
+   
+      }
+catch(error){
+setE(
+  error.message)
+} 
+        
+setload(false);     
+
+},[]);
+useEffect(()=>{
+pro();
+},[pro]);
+async function addmovieHandler(movie){
+  console.log(movie)
+  let response =await fetch('https://movie-aab57-default-rtdb.firebaseio.com/movies.json',{
+    method:'POST',
+    body:JSON.stringify(movie),
+    headers:{
+'content-type':'application/json'
+    }
+})
+let data=await response.json();
+console.log(data);
  
-  return (
-    <>
-<List></List>
-
-    </>
-  )
 }
+  return (
+    <React.Fragment>
+      <section>
+        <Form onAdd={addmovieHandler}>
+
+        </Form>
+      </section>
+      <section>
+        <button onClick={pro}>Fetch Movies</button>
+      </section>
+      <section>
+        {!load && <MoviesList movies={movie} />}
+        {load && <p>loading</p>}
+        {
+    !load && error && <p>{error}</p>
+      }
+      </section>
+    </React.Fragment>
+  );
+}
+
 export default App;
+ 
+  
